0875dfd30818eb84090dd1da42159ff856c5f99b
sunnyddelight <sunnyddelight@gmail.com>
2014-11-22 17:58:56 -0600

    Revert "changed part of game and added the html"
    This reverts commit ea24a02c9705bca50850a9f0bafb5f17a2e26253.

--- game.js
+++ game.js
@@ -1,13 +1,10 @@
 var canvas, ctx;
 var pressedKeys = [];
-var backgroundImg;
 var player=null;
-var playerWidth=100;
-var playerHeight=100;
 var enemies = [];
 var stepSize= 7;
-var uncontrolledPlayers = [];
-var globalX, globalY;
+
+
 function Player(x, y, w, h, image)
 {
     this.x = x;
@@ -15,11 +12,10 @@ function Player(x, y, w, h, image)
     this.w = w;
     this.h = h;
     this.image = image;
-    this.state = true;
 }
 Player.prototype.Draw = function()
 {
-    ctx.drawImage(this.image, this.x - this.w/2 - globalX, this.y - this.h/2 - globalY);
+    ctx.drawImage(this.image, this.x - this.w/2, this.y - h/2);
 }
 
 Player.prototype.CheckCollisions = function()
@@ -30,60 +26,29 @@ Player.prototype.CheckCollisions = function()
        {
            if(enemies[ekey] != undefined)
            {
-               var leftP = this.x - this.w/2;
-               var rightP = this.x + this.w/2;
-               var topP = this.y - this.h/2;
-               var bottomP = this.y + this.h/2;
-               var leftE = enemies[ekey].x - enemies[ekey].w/2;
-               var rightE = enemies[ekey].x + enemies[ekey].w/2;
-               var topE = enemies[ekey].y - enemies[ekey].h/2;
-               var bottomE = enemies[ekey].y + enemies[ekey].h/2;
-               
+               if(this.y - this.h/2 < enemies[ekey].y + enemies[ekey].h/2 && this.x-this.w/2 < 
            }
        }
     }
 }
-
-function UncontrolledPlayer(x, y, w, h, image)
-{
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.image = image;
-}
-
-UncontrolledPlayer.prototype.Draw = function()
-{
-    if(this.x - this.w/2 >= globalX
-}
 function Enemy(x, y, w, h, image)
 {
-    
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
     this.image = image;
+    
 }
 Enemy.prototype.Draw = function()
 {
-    
     ctx.drawImage(this.image, this.x-this.w/2, this.y);
 }
 
 function drawScene(){
     processKeys();
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.drawImage(backgroundImg, 0, 0, 700, 700, 0, 0, 700, 700);
     player.Draw();
-    if (enemies.length > 0) {
-            for (var ekey in enemies) {
-                if (enemies[ekey] != undefined) {
-                    enemeis[ekey].Draw();
-                }
-            }
-        }
     
 }
 
@@ -114,16 +79,6 @@ $(function(){
     canvas = document.getElementById('scene');
     ctx = canvas.getContext('2d');
     
-    backgroundImg=new Image();
-    backgroundImg.src = 'images/dirt.png';
-    backgroundImg.onload=function(){}
-    
-    var playerImg= new Image();
-    playerImg.src='images/player.png';
-    playerImg.onload=function(){
-        player=new Player(canvas.width /2, canvas.height- playerHeight-10, playerWidth, playerHeight, playerImg);
-    }
-    
     $(window).keydown(function (evt){ // onkeydown event handle
         var pk = pressedKeys[evt.keyCode];
         if (! pk) {
@@ -136,8 +91,6 @@ $(function(){
             delete pressedKeys[evt.keyCode]; // remove pressed key from array
         }
     });
-    
-    setInterval(drawScene, 30);
 
 
 }
